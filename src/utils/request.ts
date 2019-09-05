fdb32477fc9d757b14a1c81d7f7bebe45508b683
qiaoyaru123 <2593652689@qq.com>
2019-09-05 19:55:15 +0800

    搭建页面

---
+++ src/utils/request.ts
@@ -0,0 +1,35 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2019-09-03 20:29:10
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2019-09-05 08:45:16
+ */
+
+import axios from 'axios';
+import { AxiosResponse } from 'axios/index';
+import { getToken } from './index';
+
+
+const instance = axios.create({
+    baseURL: 'http://169.254.32.36:7001',
+    timeout: 1000,
+    headers: { 'authorization': getToken() }
+})
+
+//请求拦截器
+instance.interceptors.request.use((config: any) => {
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+})
+
+//响应拦截器
+instance.interceptors.response.use((Response: AxiosResponse<any>) => {
+    return Response.data;
+}, (error) => {
+    return Promise.reject(error);
+})
+
+export default instance;
---
+++ src/views/main/test/checkQuestion/index.tsx
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import {observer, inject} from 'mobx-react';
+import "./index.css"
+
+interface Listinfo{
+    list:Array<object>,
+    question:any,
+    data:Array<object>,
+    subject:any
+}
+
+// sumbject
+@inject('question',"subject")
+@observer
+
+class CheckQuestion extends React.Component<Listinfo> {
+    
+    state = {
+        list:[],
+        subjectList:[]
+    }
+
+    constructor(props: any){
+        super(props);
+        
+    }
+
+    componentDidMount(){
+        this.getQuestion()
+        this.getSubject()
+    }
+   getQuestion=async ()=>{
+        const {question} = this.props.question;
+        const result = await question();
+        
+        this.setState({
+            list:result.data
+        })
+    }
+
+    getSubject=async ()=>{
+        const {subject} = this.props.subject;
+        const result = await subject();
+        console.log(result)
+        this.setState({
+            subjectList:result.data
+        })
+    }
+
+    public render() {
+        const {list} = this.state
+
+        return (
+            <div className="conentPage">
+                <header className="header">查看试卷</header>
+                <div className="subjectBox">
+
+                </div>
+                <main className="main">
+                    <ul className="list-subject-item">
+                         {
+                               list&&list.map((item:any)=>{
+                                  return <li key={item.questions_id}>
+                                      <div>
+                                          <span>{item.title}</span>
+                                      </div>
+                                      <div>
+                                          <p>
+                                              <span>{item.questions_type_text}</span>
+                                              <span>{item.subject_text}</span>
+                                              <span>{item.exam_name}</span>
+                                          </p>
+                                          <p>
+                                              <span>编辑</span>
+                                          </p>
+                                      </div>
+                                      <div>
+                                          <span>{item.user_name}发布</span>
+                                      </div>
+                                  </li>
+                               })
+                         }
+                    </ul>
+                </main>
+            </div>
+            
+        )
+    }
+
+    
+}
+
+export default CheckQuestion;
